27b6e1d75f3b9d19a288f0ba2807ff5297a32220
sssujijl <leesuji1998@naver.com>
2024-01-31 18:44:27 +0900

    이력서 수정 API

--- routes/resumes.router.js
+++ routes/resumes.router.js
@@ -4,6 +4,7 @@ import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
+// 이력서 생성 API
 router.post('/resumes', authMiddleware, async (req,res,next) => {
     const {userId} = req.user;
     const {title, content, author} = req.body;
@@ -20,6 +21,7 @@ router.post('/resumes', authMiddleware, async (req,res,next) => {
     return res.status(201).json({data : resume});
 })
 
+// 모든 이력서 조회 API
 router.get('/resumes', async (req,res,next) => {
     const resumes = await prisma.resumes.findMany({
         select : {
@@ -28,6 +30,7 @@ router.get('/resumes', async (req,res,next) => {
             title : true,
             content : true,
             author : true,
+            status : true,
             createdAt : true,
             updatedAt : true
         },
@@ -39,6 +42,7 @@ router.get('/resumes', async (req,res,next) => {
     return res.status(200).json({data : resumes});
 })
 
+// 이력서 상세 조회 API
 router.get('/resumes/:resumeId', async (req,res,next) => {
     const {resumeId} = req.params;
     const resume = await prisma.resumes.findFirst({
@@ -49,12 +53,57 @@ router.get('/resumes/:resumeId', async (req,res,next) => {
             title : true,
             content : true,
             author : true,
+            status : true,
             createdAt : true,
             updatedAt : true
         }
     });
 
     return res.status(200).json({data : resume});
+})
+
+// 이력서 수정 API
+router.put('/resumes/:resumeId', async (req,res,next) => {
+    const {resumeId} = req.params;
+    const {title, content, status} = req.body;
+    const Id = await prisma.resumes.findFirst({where : {resumeId : +resumeId}});
+
+    if (!Id) {
+        return res.status(404).json({message : "이력서 조회에 실패하였습니다."});
+    };
+
+    const resume = await prisma.resumes.update({
+        where : Id,
+        select : {
+            resumeId : true,
+            userId : true,
+            title : true,
+            content : true,
+            author : true,
+            status : true,
+            createdAt : true,
+            updatedAt : true
+        },
+        data : {
+            title : title,
+            content : content,
+            status : status
+        }
+    });
+
+    return res.status(200).json({data : resume});
+})
+
+// 이력서 삭제 API
+router.delete('/resumes/:resumeId', async (req,res,next) => {
+    const {resumeId} = req.params;
+    const Id = await prisma.resumes.findFirst({where : {resumeId : +resumeId}});
+
+    if (!Id) {
+        return res.status(404).json({message : "이력서 조회에 실패하였습니다."});
+    };
+
+    
 })
 
 export default router;
